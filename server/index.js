b392719ea302264a681c97fb202803f28bb19647
jubuckley <julia.buckley@publicissapient.com>
2022-10-20 14:30:33 -0400

    basic node, react, express skeleton

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// GET reqs to the /api route
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+});
+
+// Handle all other GET requests - responds with React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
