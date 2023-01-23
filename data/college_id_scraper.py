b5fdba3bf6f7fad7b5b2605fd1235c268ae15d38
juliafbuckley <juliafbuckley@gmail.com>
2023-01-23 16:12:16 -0500

    finish college id/name/loc scraper

--- data/college_id_scraper.py
+++ data/college_id_scraper.py
@@ -46,27 +46,61 @@ class collegeIDScraper():
         self.driver.get(url)
 
         # The 'a+' attribute makes it append.
-        self.collegeid_file = open('college_ids_IN.csv', 'w+', newline='', encoding='utf-8')
+        self.collegeid_file = open('college_ids.csv', 'w+', newline='', encoding='utf-8')
         self.collegeid_writer = csv.writer(self.collegeid_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
+        
+    def navigateToNextState(self, stateIndex):
+        # If stateIndex is 60, all pages have been traversed and we return
+        if (stateIndex >= 59):
+            return
+        # Define the select object on the page
+        # TODO: stateindex = 60 breaks this part. figure out why
+        try:
+            select_state = Select(WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="ctl00_cphCollegeNavBody_ucSearchMain_ucMapMain_lstState"]'))))
+        except RecursionError:
+            print("\n\nRecursionError occurred: stateIndex {}.".format(stateIndex))
+            
+        select_state.deselect_by_index(stateIndex-1)
+        select_state.select_by_index(stateIndex)
+        
+        # Click for new results page
+        showResultsPage = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="ctl00_cphCollegeNavBody_ucSearchMain_btnSearch"]')))
+        ActionChains(self.driver).move_to_element(showResultsPage).click().perform()
+        
+        stateIndex += 1
+        
+        # Grab the first page of ids
+        self.grabIds(stateIndex)
+    
     
     # Move to the next page
-    def navigateToNextPage(self):
+    def navigateToNextPage(self, stateIndex):
         nextPageXPATH = '//*[@id="ctl00_cphCollegeNavBody_ucResultsMain_divPagingControls"]/div/a'
+
         isNextPagePresent = len(self.driver.find_elements(By.XPATH, nextPageXPATH))
+        
+        # CASE: There is only one page of results
+        if (isNextPagePresent==0):
+            self.navigateToNextState(stateIndex)
+            return
+
+        
         if (isNextPagePresent>1):
             gotoNextPage =  WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, nextPageXPATH + '[2]')))
             ActionChains(self.driver).move_to_element(gotoNextPage).click().perform()
-            self.grabIds()
-        elif (self.driver.find_element(By.XPATH, nextPageXPATH).text == 'Next Page »'):
+            self.grabIds(stateIndex)
+        elif ('Next Page' in self.driver.find_element(By.XPATH, nextPageXPATH).text):
             gotoNextPage =  WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, nextPageXPATH)))
             ActionChains(self.driver).move_to_element(gotoNextPage).click().perform()
-            self.grabIds()
+            self.grabIds(stateIndex)
+        elif stateIndex < 60:
+            self.navigateToNextState(stateIndex)
         else:
             return
 
     # Traverse the current page and grab all college IDs and names
-    def grabIds(self):
+    def grabIds(self, stateIndex):        
         # Find the results table
         resultsTable =  WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'resultsTable')))
 
@@ -90,13 +124,14 @@ class collegeIDScraper():
             self.collegeid_writer.writerow([college_id, college_name, college_loc])
 
         # Move to next page
-        self.navigateToNextPage()
-        return
+        self.navigateToNextPage(stateIndex)
 
 
 def main():
     bot = collegeIDScraper()
-    bot.grabIds()
+    # ORIGINAL CODE
+    bot.navigateToNextState(1)
+
     # Close files and driver session
     bot.collegeid_file.close()
     bot.driver.close()
